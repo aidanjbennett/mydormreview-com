dccd3e477e842914fdf77bcd526d405b606c0c88
Aidan Bennett <57372629+aidanjbennett@users.noreply.github.com>
2026-03-12 11:56:11 +0000

    First commit

---
+++ app/pages/index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+useHead({
+  title: 'My Dorm Review',
+  meta: [{ name: 'description', content: 'My amazing site.' }]
+})
+</script>
+
+<template>
+  <main>
+    <h1 class="mt-5 text-center text-3xl font-bold underline">Hello, world</h1>
+  </main>
+</template>
