5113116c71f6230794e17711eca23017cc6db3bf
Aidan <57372629+aidanjbennett@users.noreply.github.com>
2026-03-16 11:51:27 +0000

    Add Lint & Format Github Action

--- server/api/v1/uni.get.ts
+++ server/api/v1/uni.get.ts
@@ -1,6 +1,6 @@
 import { prisma } from '~~/server/prisma'
 
-export default defineEventHandler(async (event) => {
+export default defineEventHandler(async (_) => {
   const unis = await prisma.uni.findMany()
 
   return {
